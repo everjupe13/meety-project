5d338a1fba67a699d25ac3d12741df73243e445b
Ilya Bakirov <everjupe13@gmail.com>
2023-11-07 00:31:21 +0300

    feat: add button ui component

---
+++ components/shared/button/ui/Button.types.ts
@@ -0,0 +1,9 @@
+export type ButtonVariant =
+  | 'primary'
+  | 'secondary'
+  | 'outlined-secondary'
+  | 'iconed'
+
+export type ButtonFilledWith = 'icon' | 'text'
+
+export type ButtonSize = 'normal' | 'mini' | 'mini-with-big-text'
