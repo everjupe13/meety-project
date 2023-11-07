6e90da636c95040fbf30431cbe043845ba778856
Ilya Bakirov <everjupe13@gmail.com>
2023-11-07 03:11:24 +0300

    feat: update profile album page

---
+++ components/widgets/profile/ui/album/AlbumCard.tsx
@@ -0,0 +1,36 @@
+import clsx from 'clsx'
+import Image from 'next/image'
+import { FC } from 'react'
+
+type Props = {
+  className?: string
+  image?: string
+  id?: number | string
+  restricted?: boolean
+}
+const AlbumCard: FC<Props> = ({ className, image, id, restricted = false }) => {
+  return (
+    <article
+      className={clsx(
+        'relative flex items-center justify-center overflow-hidden rounded-[14px]',
+        {
+          'after:bg-purple after:absolute after:inset-[-5px] after:z-[2] after:block after:opacity-50':
+            restricted
+        },
+        className
+      )}
+    >
+      <Image
+        src={image || ''}
+        width={300}
+        height={300}
+        alt={id ? String(id) : ''}
+        className={clsx('relative z-[1] block h-[105%] w-[105%] object-cover', {
+          'blur-[10px]': restricted
+        })}
+      />
+    </article>
+  )
+}
+
+export default AlbumCard
